39e73dc973faf272d0c64097512c422c2a7e6ea7
github username <github email address>
2019-05-07 13:42:04 -0700

    added some controllers & some views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,36 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    use Rack::Flash
+    set :session_secret, "password_security"
   end
 
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect '/login'
+      end
+    end
+
   end
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  get '/users' do
+  end
+
+  get '/users/:slug' do
+  end
+
+  get '/login' do
+  end
+
+  post '/login' do
+  end
+
+  get '/signup' do
+  end
+
+  post '/signup' do
+  end
+
+  get '/logout' do
+  end
+
+end
